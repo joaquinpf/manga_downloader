7a43fa8df5a87f4f3baa8124d0fcfbafe3cffc8e
Joaquín Pérez Fuentes <joaquinpf@gmail.com>
2017-08-06 20:51:19 -0300

    Added MStream and MTown

--- src/plugins/mangatown.py
+++ src/plugins/mangatown.py
@@ -16,33 +16,34 @@ from util.util import get_source_code, fix_formatting
 
 # ####################
 
-class KissManga(SiteParserBase):
+class MangaTown(SiteParserBase):
     re_get_image_urls = re.compile(ur'lstImages\.push\("(.*)"\);')
-    re_get_max_pages = re.compile(ur'lstImages\.push\(".*/([\d.]+)\..*"\);', re.DOTALL)
+    re_get_image = re.compile('img src="([^"]*)" .* id="image" .*')
 
     def __init__(self):
-        SiteParserBase.__init__(self, 'http://kissmanga.com', 'KissManga')
+        SiteParserBase.__init__(self, 'http://www.mangatown.com', 'MangaTown')
 
     def get_manga_url(self, manga):
-        manga = fix_formatting(manga, '-', remove_special_chars=True, lower_case=True, use_ignore_chars=True, extra_ignore_chars=[':'])
-        manga = manga.replace(':', '-')
-        url = '%s/Manga/%s' % (self.base_url, manga)
+        # Example: http://www.mangatown.com/manga/one_piece/
+        manga = fix_formatting(manga, '_', remove_special_chars=True, lower_case=True, use_ignore_chars=True, extra_ignore_chars=[':', '/'])
+        manga = manga.replace(':', '_')
+        manga = manga.replace('/', '_')
+        manga = re.sub(r'_+', '_', manga)
+        url = '%s/manga/%s' % (self.base_url, manga)
         return url
 
     def parse_chapters(self, url, manga):
 
         source = get_source_code(url, config.proxy)
         soup = BeautifulSoup(source, 'html.parser')
-        r_chapters = soup.find("table", class_="listing").find_all("tr")
-        r_chapters.pop(0)
-        r_chapters.pop(0)
+        r_chapters = soup.find("ul", class_="chapter_list").find_all("li")
         chapters = [[]]
 
         for row in r_chapters:
-            info = row.find_all('td')[0].a
-            c_url = self.base_url + info['href']
+            info = row.a
+            c_url = info['href']
             chapter_line = info.get_text().strip()
-            chapter = re.sub(manga.lower().replace('-', '.'), '', chapter_line.lower()).replace('read online', '').replace('chapter', '').strip()
+            chapter = re.sub(manga.lower().replace('-', '.').replace(' ', '.'), '', chapter_line.lower().replace('-', '.')).replace('read online', '').replace('chapter', '').strip()
             chapter = re.sub("(vol\.[\d.]+)", '', chapter)
             chapter = re.sub("(:.*)", '', chapter)
             chapter = re.sub("-*\s*", '', chapter)
@@ -63,14 +64,16 @@ class KissManga(SiteParserBase):
 
     def get_max_pages(self, url):
         source = get_source_code(url, config.proxy)
-        return len(re.findall(re.compile(ur'lstImages\.push'), source))
+        soup = BeautifulSoup(source, 'html.parser')
+        return len(soup.find("div", class_="page_select").find_all("option"))
 
     def download_chapter(self, url, manga_chapter_prefix, current_chapter):
         max_pages = self.get_max_pages(url)
-        for page in range(1, max_pages + 1):
-            page_url = '%s#%i' % (url, page)
+        self.parse_image_page(1, url, manga_chapter_prefix, max_pages, current_chapter)
+        for page in range(2, max_pages + 1):
+            page_url = '%s%i.html' % (url, page)
             self.parse_image_page(page, page_url, manga_chapter_prefix, max_pages, current_chapter)
 
 #Register plugin
 def setup(app):
-    app.register_plugin('KissManga', KissManga)
+    app.register_plugin('MangaTown', MangaTown)
